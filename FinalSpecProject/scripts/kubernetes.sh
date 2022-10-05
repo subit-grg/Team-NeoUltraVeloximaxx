1f5547d8b5698f9f5aac33cc2d754ca11bd462e4
Ubuntu <ubuntu@ip-172-31-29-51.eu-west-2.compute.internal>
2022-10-05 15:05:23 +0000

    context

--- FinalSpecProject/scripts/kubernetes.sh
+++ FinalSpecProject/scripts/kubernetes.sh
@@ -1,8 +1,8 @@
-kubectl apply -f kubernetes/pass.yaml --context ProkectCluster.eu-west-2.eksctl.io
-kubectl apply -f kubernetes/backend.yaml  --context ProkectCluster.eu-west-2.eksctl.io
-kubectl apply -f kubernetes/frontend.yaml  --context ProkectCluster.eu-west-2.eksctl.io
-kubectl apply -f kubernetes/mysql.yaml  --context ProkectCluster.eu-west-2.eksctl.io
-kubectl apply -f kubernetes/nginx-configmap.yaml  --context ProkectCluster.eu-west-2.eksctl.io
-kubectl apply -f kubernetes/nginx.yaml  --context ProkectCluster.eu-west-2.eksctl.io
+kubectl apply -f kubernetes/pass.yaml --context arn:aws:eks:eu-west-2:131481335852:cluster/ProkectCluster
+kubectl apply -f kubernetes/backend.yaml  --context arn:aws:eks:eu-west-2:131481335852:cluster/ProkectCluster
+kubectl apply -f kubernetes/frontend.yaml  --context arn:aws:eks:eu-west-2:131481335852:cluster/ProkectCluster
+kubectl apply -f kubernetes/mysql.yaml  --context arn:aws:eks:eu-west-2:131481335852:cluster/ProkectCluster
+kubectl apply -f kubernetes/nginx-configmap.yaml  --context arn:aws:eks:eu-west-2:131481335852:cluster/ProkectCluster
+kubectl apply -f kubernetes/nginx.yaml  --context arn:aws:eks:eu-west-2:131481335852:cluster/ProkectCluster
 sleep 10
 kubectl get services 
