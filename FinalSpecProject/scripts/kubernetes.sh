a61a690033dba1dfaa1f7d3463793b15cc6736e1
Ubuntu <ubuntu@ip-172-31-29-51.eu-west-2.compute.internal>
2022-10-05 15:01:53 +0000

    context

--- FinalSpecProject/scripts/kubernetes.sh
+++ FinalSpecProject/scripts/kubernetes.sh
@@ -1,8 +1,8 @@
-kubectl apply -f kubernetes/pass.yaml 
-kubectl apply -f kubernetes/backend.yaml
-kubectl apply -f kubernetes/frontend.yaml 
-kubectl apply -f kubernetes/mysql.yaml
-kubectl apply -f kubernetes/nginx-configmap.yaml 
-kubectl apply -f kubernetes/nginx.yaml 
+kubectl apply -f kubernetes/pass.yaml --context ProkectCluster.eu-west-2.eksctl.io
+kubectl apply -f kubernetes/backend.yaml  --context ProkectCluster.eu-west-2.eksctl.io
+kubectl apply -f kubernetes/frontend.yaml  --context ProkectCluster.eu-west-2.eksctl.io
+kubectl apply -f kubernetes/mysql.yaml  --context ProkectCluster.eu-west-2.eksctl.io
+kubectl apply -f kubernetes/nginx-configmap.yaml  --context ProkectCluster.eu-west-2.eksctl.io
+kubectl apply -f kubernetes/nginx.yaml  --context ProkectCluster.eu-west-2.eksctl.io
 sleep 10
 kubectl get services 
