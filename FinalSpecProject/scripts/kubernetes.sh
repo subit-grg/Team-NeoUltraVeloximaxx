daefaf84ab10e559d281d2afe8f4c5ad5934d951
Ubuntu <ubuntu@ip-172-31-29-51.eu-west-2.compute.internal>
2022-10-05 14:48:23 +0000

    Jenkins kubes error

--- FinalSpecProject/scripts/kubernetes.sh
+++ FinalSpecProject/scripts/kubernetes.sh
@@ -1,8 +1,8 @@
-kubectl apply -f kubernetes/pass.yaml
-kubectl apply -f kubernetes/backend.yaml
-kubectl apply -f kubernetes/frontend.yaml
-kubectl apply -f kubernetes/mysql.yaml
-kubectl apply -f kubernetes/nginx-configmap.yaml
-kubectl apply -f kubernetes/nginx.yaml
+kubectl apply -f kubernetes/pass.yaml --context ProkectCluster
+kubectl apply -f kubernetes/backend.yaml --context ProkectCluster
+kubectl apply -f kubernetes/frontend.yaml --context ProkectCluster
+kubectl apply -f kubernetes/mysql.yaml --context ProkectCluster
+kubectl apply -f kubernetes/nginx-configmap.yaml --context ProkectCluster
+kubectl apply -f kubernetes/nginx.yaml --context ProkectCluster
 sleep 10
-kubectl get services
+kubectl get services --context ProkectCluster
