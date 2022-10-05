a4853c45a91cb6e2f97e414bcc74bb3738cef42c
Ubuntu <ubuntu@ip-172-31-29-51.eu-west-2.compute.internal>
2022-10-05 14:05:27 +0000

    jenkins

---
+++ FinalSpecProject/scripts/aws-config.sh
@@ -0,0 +1,4 @@
+aws configure set aws_access_key_id $AWS_ACCESS_KEY
+aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+aws configure set region eu-west-2
+aws configure set output json
---
+++ FinalSpecProject/scripts/installs.sh
@@ -0,0 +1,15 @@
+# initial command to update environment packages
+sudo apt update && sudo apt upgrade -y
+
+# install extra packages
+sudo apt install -y wget unzip curl
+
+# install aws cli
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+# install kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
